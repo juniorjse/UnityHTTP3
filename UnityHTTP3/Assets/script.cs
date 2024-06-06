7fc9dd3bf6dd3c3ba6b4b3b6de2187e8ba7355dc
junior.silva <junior.silva@ccc.ufcg.edu.br>
2024-06-06 19:15:59 -0300

    feat: using Stirling.MsQuic library

--- UnityHTTP3/Assets/script.cs
+++ UnityHTTP3/Assets/script.cs
@@ -1,93 +1,38 @@
 using System;
-using System.Text;
-using QuicNet;
-using QuicNet.Connections;
-using QuicNet.Streams;
 using UnityEngine;
 using UnityEngine.UI;
+using StirlingLabs.MsQuic;
+using StirlingLabs.Utilities;
 
 public class script : MonoBehaviour
 {
-    private QuicClient client;
-    private QuicConnection connection;
+    private QuicClientConnection _clientSide = null!;
 
     public Text _text;
 
-    private void Start()
+    void Start()
     {
-        InitializeClient();
+        QuicRegistration registration = new QuicRegistration("ClientTest");
+        bool reliableDatagrams = false;
+        SizedUtf8String[] alpns = new SizedUtf8String[] { SizedUtf8String.Create("h1") };
+
+        QuicClientConfiguration config = new QuicClientConfiguration(registration, reliableDatagrams, alpns);
+
+        _clientSide = new QuicClientConnection(config);
     }
 
-    private void InitializeClient()
+    public async void ButtonFunction()
     {
+        ushort port = 443;
         try
         {
-            client = new QuicClient();
-
-            connection = client.Connect("127.0.0.1", 11000);
-
-            Debug.Log("Connected to the server.");
+            await _clientSide.ConnectAsync(SizedUtf8String.Create("localhost"), port);
+            _text.text = "Conexão bem sucedida!";
         }
         catch (Exception ex)
         {
-            Debug.LogError($"Error connecting: {ex.Message}");
+            _text.text = $"Erro ao conectar: {ex.Message}";
         }
     }
 
-    public string ConnectToQuic()
-    {
-        try
-        {
-            QuicStream stream = connection.CreateStream(QuickNet.Utilities.StreamType.ClientBidirectional);
-
-            stream.Send(Encoding.UTF8.GetBytes("Hello from UnityClient!"));
-
-            byte[] data = stream.Receive();
-
-            return Encoding.UTF8.GetString(data);
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError($"Error sending/receiving data: {ex.Message}");
-            return "Communication error with the server.";
-        }
-    }
-
-    public void ButtonFunction()
-    {
-        try
-        {
-            string response = ConnectToQuic();
-            _text.text = response;
-        }
-        catch (Exception ex)
-        {
-            Debug.LogError($"Error in ButtonFunction: {ex.Message}");
-        }
-    }
-
-    private static string GenerateData(int kb)
-    {
-        StringBuilder res = new StringBuilder();
-        for (int i = 0; i < kb; i++)
-        {
-            for (int j = 0; j < 100; j++)
-            {
-                res.Append("!!!!!!!!!!");
-            }
-        }
-
-        return res.ToString();
-    }
-
-    private static string GenerateBytes(int bytes)
-    {
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < bytes; i++)
-        {
-            result.Append("!");
-        }
-
-        return result.ToString();
-    }
 }
